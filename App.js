d6f2127aa8f544a17c63c38c7723c504ff12d109
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-09-12 14:26:14 +0700

    setting up navigation

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AllExpenses from './screens/AllExpenses';
+import ManageExpenses from './screens/ManageExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+
+const Stack = createNativeStackNavigator()
+const Bottom = createBottomTabNavigator()
+
+function ExpensesOverview(){
+  return <Bottom.Navigator>
+    <Bottom.Screen name='RecentExpenses' component={RecentExpenses}/>
+    <Bottom.Screen name='AllExpenses' component={AllExpenses}/>
+  </Bottom.Navigator>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} />
+          <Stack.Screen name='ManageExpense' component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
