5874c7c7061cdce9049248e4e35dcd7e0718c4c5
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-09-13 16:29:00 +0700

    adjust style

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,21 +1,48 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useLayoutEffect } from "react";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constant/styles";
 
 export default function ManageExpenses({ route, navigation }) {
-  const expId = route.params?.expId
-  const isEdit = !!expId
+  const expId = route.params?.expId;
+  const isEdit = !!expId;
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: isEdit ? 'Edit Expense' : 'Add Expense'
-   })
-  }, [navigation, isEdit])
+      title: isEdit ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEdit]);
+
+  function deleteExpense(id) {}
 
   return (
-    <View>
-      <Text>ManageExpenses</Text>
+    <View style={styles.container}>
+      
+      {isEdit && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            name="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpense}
+          />
+        </View>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  },
+  deleteContainer: {
+    margin: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center"
+  }
+});
