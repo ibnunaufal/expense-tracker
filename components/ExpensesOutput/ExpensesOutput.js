e0a6ca6dbd684ee7974428c15f398474396c1542
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-09-14 19:27:18 +0700

    altText when null

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,15 +1,20 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constant/styles";
+import { ExpensesContext } from "../../store/expenses-context";
 
-
-export default function ExpensesOutput({ expenses, expensesPeriod }) {
+export default function ExpensesOutput({ expenses, expensesPeriod, altText }) {
+  let ctx = useContext(ExpensesContext);
+  let content = <Text>{altText}</Text>;
+  if (ctx.expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList expenses={expenses} />
+      {content}
     </View>
   );
 }
@@ -20,6 +25,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     paddingTop: 24,
     paddingBottom: 0,
-    backgroundColor: GlobalStyles.colors.primary700
-  }
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
 });
